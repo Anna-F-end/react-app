7cb5696406e6c1bf702feb855a467a15daf537ec
Anna <ankafoto@yandex.ru>
2023-11-04 21:24:15 +0300

    avatar imported and mock user added in Profile

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -1,3 +1,16 @@
+import avatarImg from "../assets/img/oldman.jpg";
 export const Profile = ()=> {
-    return <h1>Это страница Профиля</h1>
+    return (
+        <div className="row">
+            <div className="col-md-4">
+                <img src={avatarImg} alt="avatar-image" width = "100%"/>
+            </div>
+            <div className="col-md-8">
+                <h2>Василий Катков<span></span></h2>
+                <p>Id: <span>______</span></p>
+                <p>Email: <span>__________</span></p>
+            </div>    
+
+        </div>
+    );
 };
